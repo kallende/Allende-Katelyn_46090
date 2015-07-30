b91063442abd29f142c49bc5c5d3f2ef0cbc985d
kallende <katelynsoccergirl@yahoo.com>
2015-07-30 00:21:54 -0700

    hgghgf

--- Projects/Project 2/CardGame/main.cpp
+++ Projects/Project 2/CardGame/main.cpp
@@ -1,83 +1,48 @@
 /* 
  * File:   main.cpp
  * Author: Katelyn Allende 
- * Created on July 26, 2015, 9:50 AM
- * Purpose: Project 2 - Create a game - 
- *          Blackjack (modified) 
+ * Created on July 17, 2015, 2:10 PM
+ * Purpose: Midterm - Create a game - 
+ *          Blackjack
  */
 
 //System Libraries 
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <string>
-#include <fstream>
 using namespace std;
-
-struct input {
-    char name[50];
-};
 //User Libraries 
 
 //Global Constants 
 
 //Function Prototypes 
-void shuffle(bool *);
+void shuffle(bool []);
 void PrntCrd(int);
-void PrntHnd(int *, const int);
-int NxtCard(bool *);
+void PrntHnd(int [], const int);
+int NxtCard(bool []);
 int ScrHnd(int [], const int);
-void PrntSaH(int [], const int &, int [], const int &);
-float NewPot (bool, float, float);
+void PrntSaH(int [], const int, int [], const int);
 
 //Execution Begins Here! 
+
 int main(int argc, char** argv) {
-    //Declare Variables
-    bool CrdsDlt[52];                 //Deck of Cards 
-    int hCrdCnt = 0, pCrdCnt = 0;     //House & Player Card Count 
-    int*HseHnd=new int[12];           //Dynamic Array of House Hand 
-    int*PlyrHnd=new int[12];          //Dynamic Array of Player Hand
-    bool doAgain=0;                   //Decision to play again or not 
-    char pChoice=0;                   //Choice to Hit or Stay 
+    //Declare Variables 
+    bool CrdsDlt[52];         	      //Deck of Cards 
+    int hCrdCnt = 0;	              //House Card Count 
+    int HseHnd[12];                   //Array of House Hand 
+    int pCrdCnt = 0;                  //Player Card Count 
+    int PlyrHnd[12];                  //Array of Player Hand 
+    bool doAgain;                     //Decision to play again or not 
+    char pChoice;                     //Choice to Hit or Stay 
     bool pHits = true;                //Player Hits 
-    int pScore=0, hScore=0;           //Player's & House's Score 
-    bool hBusts=0;                    //House Bust
-    int Pot=0;                        //Pot Amount 
-    int betAmnt=0;                    //Bet Amount 
-    
-    //Welcome to Blackjack
-    //Pulling from a file 
-    string line; 
-    ifstream inFile; 
-    inFile.open("instrct.dat");
-    if (inFile.is_open()){
-        
-    while (getline (inFile,line)){
-        cout << line << endl; 
-    }
-    inFile.close(); 
-    } 
-    
-    input i;   //Structure to output name 
-    i.name;
-    cout << "Enter player's full name." << endl; 
-    cin.getline(i.name, 50);
-  
-    //Starting Pot Amount 
-    cout << endl << "How much is your starting pot?" << endl;
-    cin >> Pot; 
-        //Input Validation 
-        while (Pot<0){
-            cout << "Starting pot must be a positive value. Please re-enter value." << endl; 
-            cin >> Pot; 
-        }
-        
-        //Loop for each hand 
-        do {
-            //Pot Amount Validation 
-            if (Pot>0) {  
-        //Set the random number seed
-        srand(time(0));
+    int pScore;                       //Player's Score 
+    int hScore;                       //House's Score 
+    bool hBusts;                      //House Bust 
+
+    //Set the random number seed
+    srand(time(0));
+    //Loop for each hand
+    do {
         //Shuffle the cards 
         shuffle(CrdsDlt);
 
@@ -91,19 +56,7 @@ int main(int argc, char** argv) {
 
         //Heading for a new hand 
         cout << "******************* New Hand *******************" << endl;
-        
-        //Bet Amount 
-        cout << "How much would you like to bet?" << endl;
-        cin >> betAmnt;
-            //Input Validation 
-            while (betAmnt>Pot){
-                cout << "You cannot bet more than what is in your pot. Please re-enter bet." << endl;
-                cin >> betAmnt; 
-                }
-        cout << endl; 
-            //Input Validation 
-            if (betAmnt>=5 && betAmnt<=100) {
-            
+
         //Display first two cards 
         do {
             cout << "House's Hand" << endl;
@@ -112,28 +65,20 @@ int main(int argc, char** argv) {
             cout << endl;
             cout << "Player's Hand Score: " << ScrHnd(PlyrHnd, pCrdCnt) << endl;
             PrntHnd(PlyrHnd, pCrdCnt);
-        
+
             //Ask player if they would like to hit or stay
             cout << "Would you like to hit(h) or stay(s)?" << endl;
             cin >> pChoice;
-            if (pChoice == 'h'||'s'){
-                switch(pChoice)
-                {
-                    case 'h':
-                        PlyrHnd[pCrdCnt] = NxtCard(CrdsDlt);
-                        ++pCrdCnt;
-                        break; 
-                    case 's':
-                        pHits=false; 
-                        break;  
-                    default: 
-                        cout << endl << "Invalid input. Try Again." << endl; 
-                } 
-            }else {
+            if (pChoice == 'h') {
+                PlyrHnd[pCrdCnt] = NxtCard(CrdsDlt);
+                ++pCrdCnt;
+            } else if (pChoice == 's') {
+                pHits = false;
+            } else {
                 cout << "Invalid input. Try Again." << endl;
-                }
+            }
             cout << endl;
-             
+
             //Update Score, Check for Bust, and See Who Wins 
             pScore = ScrHnd(PlyrHnd, pCrdCnt);
             }while (pHits && pScore < 22);
@@ -141,14 +86,10 @@ int main(int argc, char** argv) {
                     cout << "-------------------------" << endl;
                     cout << "THE HOUSE WINS!!!" << endl;
                     PrntSaH(HseHnd, hCrdCnt, PlyrHnd, pCrdCnt);
-                    Pot = NewPot(false, Pot, betAmnt);
-                    cout << "Your total: " << Pot << endl; 
                 }else if (pScore==21){
                     cout << "-------------------------" << endl;
                     cout << "THE PLAYER WINS!!!" << endl;
                     PrntSaH(HseHnd, hCrdCnt, PlyrHnd, pCrdCnt);
-                    Pot = NewPot(true, Pot, betAmnt);
-                    cout << "Your total: " << Pot << endl;        
                 }else {
                     hScore = ScrHnd(HseHnd, hCrdCnt);
                     while (hScore < 17) {
@@ -161,60 +102,35 @@ int main(int argc, char** argv) {
                         cout << "-------------------------" << endl;
                         cout << "THE PLAYER WINS!!!" << endl;
                         PrntSaH(HseHnd, hCrdCnt, PlyrHnd, pCrdCnt);
-                        Pot = NewPot(true, Pot, betAmnt);
-                        cout << "Your total: " << Pot << endl;  
                     }else {
                         if (pScore == hScore) {
                             cout << "-------------------------" << endl;
                             cout << "TIE!!!" << endl;
                             PrntSaH(HseHnd, hCrdCnt, PlyrHnd, pCrdCnt);
-                            cout << "Your total: " << Pot << endl; 
                         }else if (pScore > hScore) {
                             cout << "-------------------------" << endl;
                             cout << "THE PLAYER WINS!!!" << endl;
                             PrntSaH(HseHnd, hCrdCnt, PlyrHnd, pCrdCnt);
-                            Pot = NewPot(true, Pot, betAmnt);
-                            cout << "Your total: " << Pot << endl; 
                         }else {
                             cout << "-------------------------" << endl;
                             cout << "THE HOUSE WINS!!!" << endl;
                             PrntSaH(HseHnd, hCrdCnt, PlyrHnd, pCrdCnt);
-                            Pot = NewPot(false, Pot, betAmnt);
-                            cout << "Your total: " << Pot << endl; 
                         }
                     }
                 }
-            } else {
-                //Input Validation 
-                cout << "Bet minimum of $5 and maximum of $100." << endl;
-                return 0; 
-            } 
-            } else {
-                //No more money 
-                cout << "You have no more money! GAME OVER!" << endl; 
-                cin.ignore();
-                cout << "BYE " << i.name << "!!!" << endl; 
-                return 0; 
-            }        
- 
+
         //Prompt if they would like to continue
         cout << endl << "Would you like to continue y/n?" << endl;
         char response;
         cin >> response;
         cout << endl;
         if (response == 'y') doAgain = true;
-        else {
-            doAgain = false;
-            cout << "Your cash out amount = $" << Pot << endl; 
-            cin.ignore();
-            cout << "BYE " << i.name << "!!!" << endl;  
-            }
-        } while (doAgain);   
-        delete HseHnd;
-        delete PlyrHnd;
-        
+        else doAgain = false;
+    } while (doAgain);
+
     return 0;
 }
+
 /*******************************************************************
  ***************************** PrntSaH *****************************
  *******************************************************************
@@ -228,7 +144,7 @@ int main(int argc, char** argv) {
  *          ScrHnd    ->  Score Hand for House and Player 
  *          PrntHnd   ->  Print Hand for House and Player 
  ********************************************************************/
-void PrntSaH(int *HseHnd, const int &hCnt, int *PlyrHnd, const int &pCnt) {
+void PrntSaH(int HseHnd[], const int hCnt, int PlyrHnd[], const int pCnt) {
     //Output each score and the hand
     cout << "House's Hand Score: " << ScrHnd(HseHnd, hCnt) << endl;
     PrntHnd(HseHnd, hCnt);
@@ -236,6 +152,7 @@ void PrntSaH(int *HseHnd, const int &hCnt, int *PlyrHnd, const int &pCnt) {
     PrntHnd(PlyrHnd, pCnt);
     cout << endl;
 }
+
 /*******************************************************************
  ***************************** ScrHnd ******************************
  *******************************************************************
@@ -248,10 +165,10 @@ void PrntSaH(int *HseHnd, const int &hCnt, int *PlyrHnd, const int &pCnt) {
  ********************************************************************/
 int ScrHnd(int h[], const int t) {
     //Declare Variables 
-    int AceCnt = 0;      //Ace Count 
+    int AceCnt = 0;    //Ace Count 
     int Score = 0;
-    int newCrd=0;        //New Card 
-    int iRank=0;         //Rank of Card 
+    int newCrd;        //New Card 
+    int iRank;         //Rank of Card 
 
     //For-loop to determine score of hand 
     for (int r = 0; r < t; r++) {
@@ -272,6 +189,7 @@ int ScrHnd(int h[], const int t) {
     }
     return Score;
 }
+
 /*******************************************************************
  ***************************** NxtCrd ******************************
  *******************************************************************
@@ -281,7 +199,7 @@ int ScrHnd(int h[], const int t) {
  * Output:
  *          newCard    ->  Next card(s)
  ********************************************************************/
-int NxtCard(bool *CrdsDlt) {
+int NxtCard(bool CrdsDlt[]) {
     //Declare Variable
     int newCard = -1;
     bool d = true;
@@ -295,6 +213,7 @@ int NxtCard(bool *CrdsDlt) {
     } while (d);
     return newCard;
 }
+
 /*******************************************************************
  ***************************** PrntHnd *****************************
  *******************************************************************
@@ -305,7 +224,7 @@ int NxtCard(bool *CrdsDlt) {
  * Input-Output:
  *          On Screen 
  ********************************************************************/
-void PrntHnd(int *h, const int t) {
+void PrntHnd(int h[], const int t) {
     //For-loop to print hand of cards 
     for (int r = 0; r < t; r++) {
         const int iNxtCrd = h[r];
@@ -314,6 +233,7 @@ void PrntHnd(int *h, const int t) {
     }
     cout << endl;
 }
+
 /*******************************************************************
  ***************************** PrntCrd *****************************
  *******************************************************************
@@ -353,6 +273,7 @@ void PrntCrd(int p) {
         cout << 'S';
     }
 }
+
 /*******************************************************************
  ***************************** shuffle *****************************
  *******************************************************************
@@ -362,31 +283,9 @@ void PrntCrd(int p) {
  * Input-Output:
  *          bool CrdsDlt   ->   Shuffled cards 
  ********************************************************************/
-void shuffle(bool *CrdsDlt) {
+void shuffle(bool CrdsDlt[]) {
     //For-loop to shuffle cards 
     for (int i = 0; i < 52; i++) {
         CrdsDlt[i] = false;
     }
 }
-/*******************************************************************
- ***************************** NewPot ******************************
- *******************************************************************
- * Purpose:     To update total amount in pot. 
- * Input:
- *          win        ->   Player won   
- *          sAmnt      ->   Original pot amount 
- *          bet        ->   Amount bet by player    
- * Output:
- *          newPot     ->   New pot amount for player  
- ********************************************************************/
-float NewPot(bool win, float sAmnt, float bet) {
-    //Declare Variables 
-    float newPot = 0.0f;        //Updated Pot Amount
-    
-    //Conditional Operator to calculate for new pot
-    (win)?(newPot=sAmnt+(bet*2)):(newPot=sAmnt-bet);
-    return newPot; 
-}
-
-
- 
